78d518551c97b55e2f216ec5595a46b1a54b1d9e
codeboy <2685312867@qq.com>
2021-11-07 11:33:38 +0800

    Initial Commit

---
+++ CBAsyncDraw/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CBAsyncDraw
+//
+//  Created by hicodeboy on 2021/11/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
